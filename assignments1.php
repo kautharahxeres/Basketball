4c4d8d25c72b8a43876d75592e5d67fbddc2b6f3
kautharahxeres <kautharahnakazibwe7@gmail.com>
2023-12-21 13:34:30 +0300

    added 3 images

---
+++ assignments1.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "hello world";
+$x="5";
+$y="John";
+
+echo $x;
+echo $y;
+?>
+<html>
+    <body>
+
+        <?php
+        $x =20;
+        $x += 100;
+
+        echo $x;
+        ?>
+        
+    </body>
+</html>
